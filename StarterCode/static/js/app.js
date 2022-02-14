d06ec2e68ea8db4e81bf2c40df5dde4e08ec8044
bakera <bakera@alumni.usc.edu>
2022-02-13 17:04:15 -0800

    updated charts

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,11 +1,22 @@
+function init() {
+  d3.json("../../samples.json").then(data => {
+      let names = data.names;
+      let selector = d3.select("#selDataset");
+      names.forEach(sample => {
+      selector.append("option").text(sample).property("value", sample);
+      });
+  });
+};
+init();
+
 function chartBuild(sample) {
   // 1. Use the D3 library to read in `samples.json`.
   d3.json("../../samples.json").then(data => {
     // console.log(data);
     // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
     let samples = data.samples;
-    let arrayMain = samples.filter(item => item.id == sample);
-    let array1 = arrayMain[0]
+    let arrayChart = samples.filter(item => item.id == sample);
+    let array1 = arrayChart[0]
     let otuIds = array1.otu_ids;
     let otuLabels = array1.otu_labels;
     let sampleValues = array1.sample_values;
@@ -23,8 +34,8 @@ function chartBuild(sample) {
     let layoutBar = {
       title:  `Top 10 OTU IDs for ${sample}`,
       margin: {
-        t:20, 
-        b: 20, 
+        t:35, 
+        b: 35, 
         l: 100, 
         r: 100
     }};
@@ -49,22 +60,38 @@ function chartBuild(sample) {
       title: `Top 10 Bacteria Strains for Sample: ${sample}`
     };
     Plotly.newPlot("bubble", bubbleChart, layoutBubble);
-    // 5. Display each key-value pair from the metadata JSON object somewhere on the page.
-    function init() {
-        let names = data.names;
-        let selector = d3.select("#selDataset");
-        names.forEach(sample => {
-          selector.append("option").text(sample).property("value", sample);
-        });
-        let sampleOne = names[0];
-        chartBuild(sampleOne);
-    }
   });
-  // 6. Update all of the plots any time that a new sample is selected.
-  function optionChanged(sampleNew) {
-    chartBuild(sampleNew);
-    console.log(`This is new sample: ${sampleNew}`);
-  }
-  init();
 };
+// function demographicBuild(sample) {
+//   // 1. Use the D3 library to read in `samples.json`.
+//   d3.json("../../samples.json").then(data => {
+//     let demographics = data.metadata;
+//     console.log(demographics);
+//     let arrayDemo = demographics.filter(item => item.id == sample);
+//     let array2 = arrayDemo[0];
+//     let ethnicity = `Ethnicity: ${array2.ethnicity}`;
+//     let gender = `Gender: ${array2.gender}`;
+//     let age = `Age: ${array2.age}`;
+//     let location = `Location: ${array2.location}`;
+//     let bbtype = `Belly Button Type: ${array2.bbtype}`;
+//     let wfreq = `Wash Frequency: ${array2.wfreq}`;
+//     let demographicList = [{
+//       ethnicity, 
+//       gender,
+//       age, 
+//       location, 
+//       bbtype, 
+//       wfreq
+//     }];
+//     let demoSelector = d3.select("#sample-metadata");
+//     demoSelector.append("option").text(demographicList).property("value", sample);
 
+//   });
+// };
+
+
+function optionChanged(sampleNew) {
+  chartBuild(sampleNew);
+  // demographicBuild(sampleNew);
+  console.log(`This is new sample: ${sampleNew}`);
+};
