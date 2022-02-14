7dc1c2e3326f30ca2e376a7c0f65107dc66dcca6
bakera <bakera@alumni.usc.edu>
2022-02-13 18:07:23 -0800

    added initial demographics table code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -23,8 +23,8 @@ function chartBuild(sample) {
     let yBar = otuIds.map(yBar => `OTU ${yBar}`);
     let barGraph = [
       {
-        x: sampleValues, // * Use `sample_values` as the values for the bar chart.
-        y: yBar.slice(0,10), // * Use `otu_ids` as the labels for the bar chart.
+        x: sampleValues.slice(0,10), // * Use `sample_values` as the values for the bar chart.
+        y: yBar, // * Use `otu_ids` as the labels for the bar chart.
         text: otuLabels, // * Use `otu_labels` as the hovertext for the chart.
         type: "bar",
         orientation: "h"
@@ -62,36 +62,39 @@ function chartBuild(sample) {
     Plotly.newPlot("bubble", bubbleChart, layoutBubble);
   });
 };
-// function demographicBuild(sample) {
-//   // 1. Use the D3 library to read in `samples.json`.
-//   d3.json("../../samples.json").then(data => {
-//     let demographics = data.metadata;
-//     console.log(demographics);
-//     let arrayDemo = demographics.filter(item => item.id == sample);
-//     let array2 = arrayDemo[0];
-//     let ethnicity = `Ethnicity: ${array2.ethnicity}`;
-//     let gender = `Gender: ${array2.gender}`;
-//     let age = `Age: ${array2.age}`;
-//     let location = `Location: ${array2.location}`;
-//     let bbtype = `Belly Button Type: ${array2.bbtype}`;
-//     let wfreq = `Wash Frequency: ${array2.wfreq}`;
-//     let demographicList = [{
-//       ethnicity, 
-//       gender,
-//       age, 
-//       location, 
-//       bbtype, 
-//       wfreq
-//     }];
-//     let demoSelector = d3.select("#sample-metadata");
-//     demoSelector.append("option").text(demographicList).property("value", sample);
-
-//   });
-// };
+function demographicBuild(sample) {
+  // 1. Use the D3 library to read in `samples.json`.
+  d3.json("../../samples.json").then(data => {
+    let demographics = data.metadata;
+    console.log(demographics);
+    let arrayDemo = demographics.filter(item => item.id == sample);
+    let array2 = arrayDemo[0];
+    let ethnicity = `Ethnicity: ${array2.ethnicity}`;
+    let gender = `Gender: ${array2.gender}`;
+    let age = `Age: ${array2.age}`;
+    let location = `Location: ${array2.location}`;
+    let bbtype = `Belly Button Type: ${array2.bbtype}`;
+    let wfreq = `Wash Frequency: ${array2.wfreq}`;
+    
+    let demographicList = [
+      ethnicity, 
+      gender,
+      age, 
+      location, 
+      bbtype, 
+      wfreq
+    ];
+    // // .append("p").text(keys[i] + ": " + values[i]);}
+    let demoSelector = d3.select("#sample-metadata");
+    for(let i =0; i< demographicList.length; i++){
+      demoSelector.append("p").text(demographicList[i]) //.property("value", sample)
+    };
+  });
+};
 
 
 function optionChanged(sampleNew) {
   chartBuild(sampleNew);
-  // demographicBuild(sampleNew);
+  demographicBuild(sampleNew);
   console.log(`This is new sample: ${sampleNew}`);
 };
